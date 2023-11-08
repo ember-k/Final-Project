b181fb5fb025707c1cd5cfb72d50b0f5a841f2a9
ember-k <emberk@udel.edu>
2023-11-08 15:03:26 -0500

    Got the left to right movement working

--- Final Project Code.py
+++ Final Project Code.py
@@ -4,7 +4,6 @@ from random import randint
 HOOK_SPEED = 10
 
 """
-- [ ] Play screen: the opening window with a high score counter and a play button
 - [ ] Setting: Program the aquatic background
 - [x] Hook exists: it exists on the screen
   of the screen and remain vertically centered for the remainder of the game.
@@ -17,37 +16,57 @@ HOOK_SPEED = 10
 @dataclass
 class World:
     hook: DesignerObject
-    water: DesignerObject
     hook_move_left: bool
     hook_move_right: bool
     hook_speed: int
 
+
 def create_world() -> World:
     """Create the world"""
-    return World(create_background(), create_hook(), False, False, HOOK_SPEED)
+    return World(create_hook(), False, False, HOOK_SPEED)
+"""
 def create_background() -> DesignerObject:
-    """Create the hook"""
+    ""Create the water""
     water = rectangle("cornflowerblue", get_window_width(), get_window_height())
     return water
+    """
 
 def create_hook() -> DesignerObject:
     """Create the hook"""
-    hook = image("https://images.emojiterra.com/openmoji/v13.1/512px/1fa9d.png", get_height() * (1/3), get_width() * (1/3))
-    hook.flip_x = True
+    hook = image("https://images.emojiterra.com/openmoji/v13.1/512px/1fa9d.png")
+    shrink(hook, 3)
+    hook.anchor = "center"
     return hook
 
 def move_hook(world: World, direction: int):
     """Move the hook horizontally"""
     world.hook.x += world.hook_speed * direction
 
+def hook_at_left_edge(world: World):
+    if world.hook.x < 0:
+        world.hook_move_left = False
+        return True
+    else:
+        return False
+def hook_at_right_edge(world: World):
+    """Handle the hook when it reaches the right wall of the window"""
+    if world.hook.x > get_width():
+        world.hook_move_right = False
+        return True
+    else:
+        return False
+
 def set_direction(world: World):
     """Change direction of hook path according to arrow keys"""
-    if world.hook_move_left:
+    if world.hook_move_left and (not hook_at_left_edge(world)):
         move_hook(world, -1)
-    elif world.hook_move_right:
+    elif world.hook_move_right and (not hook_at_right_edge(world)):
         move_hook(world, 1)
     else:
         move_hook(world, 0)
+
+
+
 
 def keys_down(world:World, key:str):
     if key == "left":
@@ -64,7 +83,9 @@ def keys_realeased(world:World, key:str):
 
 
 
-when('starting', create_background)
+
+
+
 when('starting', create_world)
 when('typing', keys_down)
 when('done typing', keys_realeased)
